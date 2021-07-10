5a8fc75607e433b3d13f3927e8766ce52be92aed
haolly <andyhaolly@gmail.com>
2021-07-10 16:55:24 +0800

    fix error

--- Dependencies.lua
+++ Dependencies.lua
@@ -25,7 +25,7 @@ Library["SPIRV_Cross_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/spirv-cross-cored.
 Library["SPIRV_Cross_GLSL_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/spirv-cross-glsld.lib"
 Library["SPIRV_Tools_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/SPIRV-Toolsd.lib"
 
-Library["ShaderC_Release"] = "%{LibraryDir.VulkanSDK_Debug}/shaderc_shared.lib"
-Library["ShaderC_Utils_Release"] = "%{LibraryDir.VulkanSDK_Debug}/shaderc_util.lib"
-Library["SPIRV_Cross_Release"] = "%{LibraryDir.VulkanSDK_Debug}/spirv-cross-core.lib"
-Library["SPIRV_Cross_GLSL_Release"] = "%{LibraryDir.VulkanSDK_Debug}/spirv-cross-glsl.lib"
+Library["ShaderC_Release"] = "%{LibraryDir.VulkanSDK}/shaderc_shared.lib"
+Library["ShaderC_Utils_Release"] = "%{LibraryDir.VulkanSDK}/shaderc_util.lib"
+Library["SPIRV_Cross_Release"] = "%{LibraryDir.VulkanSDK}/spirv-cross-core.lib"
+Library["SPIRV_Cross_GLSL_Release"] = "%{LibraryDir.VulkanSDK}/spirv-cross-glsl.lib"
